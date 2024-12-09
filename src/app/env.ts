7775f1278d4f85fd78fd89e12958c1885505539e
vikasutf8 <21bcs241@iiitdmj.ac.in>
2024-12-09 19:44:42 +0530

    setup project as appwrite

---
+++ src/app/env.ts
@@ -0,0 +1,7 @@
+export const env ={
+    appwrite :{
+        endpoint :String(process.env.NEXT_PUBLIC_APPWRITE_HOST_URI),
+        projectId :String(process.env.NEXT_PUBLIC_APPWRITE_POREJECT_ID),
+        apikey :String(process.env.NEXT_PUBLIC_APPWRITE_PRIVATE_KEY)
+    }
+}
